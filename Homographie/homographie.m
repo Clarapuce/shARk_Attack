02d7f0983408a55611a025e47d03ca9ce9785a43
Alexandra Pometko <34752616+alexandrapometko@users.noreply.github.com>
2018-10-24 17:51:10 +0200

    methode sans la boucle for, non fonctionelle

--- Homographie/homographie.m
+++ Homographie/homographie.m
@@ -18,25 +18,46 @@ img = imread('pinkshark.png');
 coinsImage = [0;0;0;lImg;hImg;lImg;hImg;0];
 H = TrouveH(coinsVideo, coinsImage);
 
-for i = 1:hVid
-    for j = 1:lVid
-        A = [j;i];
-        AH = homogeneisator(A);
-        BH = H*AH ;
-        B = dehomogeneisator(BH);
-        B = int16(B);
-        
-        Bx = B(1);
-        By = B(2);
-        
-        if (Bx <= lImg) && (Bx > 0) && (By > 0) && (By <= hImg) %si le point est dans l'image
-            %on remplace par le pixel de la vidéo par le pixel de l'image
-            frame(i,j,1) = img(Bx,By,1); %récupère le RGB normalement 
-            frame(i,j,2) = img(Bx,By,2);
-            frame(i,j,3) = img(Bx,By,3);
-        end          
-        
-    end
-end
+[Ax,Ay] = meshgrid(1:lVid, 1:hVid);
+Ax = reshape(Ax,[1 lVid*hVid]);
+Ay = reshape(Ay,[1 lVid*hVid]);
+S1=ones(1,lVid*hVid);
+A = [Ax;Ay;S1];
+
+B = H * A;
+Bx = B(1,:);
+By = B(2,:);
+S2 = B(3,:);
+Bx = int16(Bx ./ S2);
+By = int16(By ./ S2);
+pos = find((Bx>0).*(Bx <= hImg).*(By >0).*(By<=lImg)); %position de tout ceux qui respectent ces conditions
+Bx = Bx(pos);
+By = By(pos);
+Ax = Ax(pos);
+Ay = Ay(pos);
+    
+frame(Ay + (Ax-1) * hVid) = img(By + (Bx -1) * hImg); %premier plan, rouge
+frame(Ay + (Ax-1) * hVid + hVid*lVid) = img(By + (Bx -1) * hImg+hImg*lImg); %on rajoute la taille de l'image pour passer au plan vert
+frame(Ay + (Ax-1) * hVid + 2*hVid*lVid) = img(By + (Bx -1) * hImg+2*hImg*lImg); %on rajoute deux fois la taille de l'image pour passer au plan vert
+% for i = 1:hVid
+%     for j = 1:lVid
+%         A = [j;i];
+%         AH = homogeneisator(A);
+%         BH = H*AH ;
+%         B = dehomogeneisator(BH);
+%         B = int16(B);
+%         
+%         Bx = B(1);
+%         By = B(2);
+%         
+%         if (Bx <= lImg) && (Bx > 0) && (By > 0) && (By <= hImg) %si le point est dans l'image
+%             %on remplace par le pixel de la vidéo par le pixel de l'image
+%             frame(i,j,1) = img(Bx,By,1); %récupère le RGB normalement 
+%             frame(i,j,2) = img(Bx,By,2);
+%             frame(i,j,3) = img(Bx,By,3);
+%         end          
+%         
+%     end
+% end
 
 image(frame)
